67abd44472b3a65c3769df5f34d1c6f9ee78a511
Anik7200 <anik.singularity@gmail.com>
2022-09-09 01:17:37 +0600

    refactored it

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const DBCluster = process.env.DATABASE;
-const DBLocal = process.env.DATABASE_LOCAL;
+// const DBLocal = process.env.DATABASE_LOCAL;
 
 let DB_URL = DBCluster;
 
 DB_URL = DB_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-DB_URL = DB_URL.replace('<DB_NAME>', process.env.DB_NAME);
+//DB_URL = DB_URL.replace('<DB_NAME>', process.env.DB_NAME);
 
-if (process.argv[2] && process.argv[2] === 'dblocal')
-  DB_URL = DBLocal;
+// if (process.argv[2] && process.argv[2] === 'dblocal')
+//   DB_URL = DBLocal;
 
-console.log(`DB_URL`, DB_URL);
+// console.log(`DB_URL`, DB_URL);
 
 module.exports = () => {
   console.log('connecting to DB...');
